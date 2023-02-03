d6f96035dac9c421d102c9a19512069b9fc8e19f
DaianaChile <daiarocio@outlook.com>
2023-02-03 20:03:49 -0300

    terminando edits

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PortfolioAlanCejas'`, () => {
+  it(`should have as title 'PortfolioDaianaChile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PortfolioAlanCejas');
+    expect(app.title).toEqual('PortfolioDaianaChile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PortfolioAlanCejas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PortfolioDaianaChile app is running!');
   });
 });
